7d54fe73ed3dbbffa0e6a49973b73497471b9564
ZAKI <156739195+zinen33@users.noreply.github.com>
2024-11-30 02:47:00 -0800

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,10 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-		buildInputs = [
-				pkgs.nodejs-20_x
-				pkgs.nodePackages.typescript-language-server
-				pkgs.yarn
-				pkgs.nodePackages.jest
-		];
+  buildInputs = [
+    pkgs.nodejs-20_x
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.nodePackages.jest
+  ];
 }
